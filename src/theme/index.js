7b4a5dbd017de11c380d490b8c295de3003f34fc
Thant Ko Zaw <thantmkou10@gmail.com>
2022-08-01 20:32:13 +0630

    Setup themes

---
+++ src/theme/index.js
@@ -0,0 +1,31 @@
+import GlobaStyles from './globalStyles';
+
+const theme = {
+  colors: {
+    bodyBg: '#19191D',
+    primary: {
+      300: '#A5B4FC',
+      400: '#818CF8',
+      900: '#272460',
+    },
+    neutral: {
+      200: '#E5E5E5',
+      300: '#d4d4d4',
+      400: '#a3a3a3',
+      500: '#737373',
+      600: '#525252',
+    },
+    text: '#C2C2C2',
+    white: '#ffffff',
+  },
+  fontFamily: {
+    primary: "'Poppins', sans-serif",
+  },
+  breakpoints: {
+    md: '47.9375em',
+    lg: '89.9375em',
+  },
+};
+
+export default theme;
+export { GlobaStyles };
