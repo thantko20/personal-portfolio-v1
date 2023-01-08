8a203158371aba42e8d540337fe7c86cda903e27
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-08 14:50:30 +0630

    Fix error in meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,13 +10,17 @@ export default function MyApp({ Component, pageProps }) {
         <Head>
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta name='theme-color' content='#000000' />
+          <meta name='title' content='Thant Ko Zaw - Fullstack Developer' />
           <meta
             name='description'
             content='A fullstack developer based in Myanmar who loves building web apps.'
           />
           <meta property='og:type' content='website' />
           <meta property='og:url' content='https://www.thantko.dev' />
-          <meta property='og:title' content='Thant Ko Zaw' />
+          <meta
+            property='og:title'
+            content='Thant Ko Zaw - Fullstack Developer'
+          />
           <meta
             property='og:description'
             content='A fullstack developer based in Myanmar who loves building web apps.'
@@ -24,16 +28,16 @@ export default function MyApp({ Component, pageProps }) {
           <meta property='og:image' content='/landing.png' />
           <meta property='twitter:card' content='summary_large_image' />
           <meta property='twitter:url' content='https://www.thantko.dev' />
-          <meta property='twitter:title' content='Thant Ko Zaw' />
+          <meta
+            property='twitter:title'
+            content='Thant Ko Zaw - Fullstack Developer'
+          />
           <meta
             property='twitter:description'
             content='A fullstack developer based in Myanmar who loves building web apps.'
           />
-          <meta
-            property='twitter:image'
-            content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
-          ></meta>
-          <title>Thant Ko</title>
+          <meta property='twitter:image' content='/landing.png' />
+          <title>Thant Ko Zaw - Fullstack Developer</title>
         </Head>
         <GlobalStyles />
         <ScrollProvider>
