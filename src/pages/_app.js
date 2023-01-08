bba8de6d22ad1f4c6370ea0672632e5cfc08b069
Thant Ko Zaw <thantmkou10@gmail.com>
2023-01-08 13:45:05 +0630

    Fix link

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -21,7 +21,10 @@ export default function MyApp({ Component, pageProps }) {
             property='og:description'
             content='A fullstack developer based in Myanmar who loves building web apps.'
           />
-          <meta property='og:image' content='/landing.png' />
+          <meta
+            property='og:image'
+            content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
+          />
           <meta property='twitter:card' content='summary_large_image' />
           <meta property='twitter:url' content='https://www.thantko.dev' />
           <meta property='twitter:title' content='Thant Ko Zaw' />
